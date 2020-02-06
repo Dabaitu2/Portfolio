c22bae2f84b7cca975f4aa2366eda56e7904c564
tomokokawase <331555765@qq.com>
2020-02-06 17:40:34 +0800

    DOOOONEsssaaab

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,112 +11,112 @@ class MyDocument extends Document {
             rel={'preload'}
             href={`${IP}/Sofia Pro Black.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Black Italic.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Bold.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Bold Italic.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Extra Light.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Extra Light Italic.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Italic.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Light.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Light Italic.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Medium.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Medium Italic.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Regular.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Semi Bold.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Semi Bold Italic.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Semi Ultra Light.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
           <link
             rel={'preload'}
             href={`${IP}/Sofia Pro Semi Ultra Light.otf`}
             as="font"
-            type={'font/otf'}
+            type={'font/opentype'}
             crossOrigin="anonymous"
           />
         </Head>
