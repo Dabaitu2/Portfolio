0b5c90e3cf0ee31ff3946e7fd9e7eb6c0f7135d8
zhanghaoyang <zhanghaoyang.tomoko@bytedance.com>
2020-03-13 16:07:30 +0800

    + video

---
+++ .next/static/webpack/static/development/pages/index.js.ba1b3b4a55bffd851e94.hot-update.js
@@ -0,0 +1,169 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Profile/index.tsx":
+/*!**************************************!*\
+  !*** ./components/Profile/index.tsx ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _Banner__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Banner */ "./components/Profile/Banner/index.tsx");
+/* harmony import */ var _Contact__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Contact */ "./components/Profile/Contact/index.tsx");
+/* harmony import */ var _common_Pharse__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../common/Pharse */ "./components/common/Pharse/index.tsx");
+var _jsxFileName = "/Users/tomokokawase/Desktop/Portfolio/components/Profile/index.tsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0__["createElement"];
+
+
+
+
+var myInfo = [{
+  title: 'EDUCATION',
+  info: [{
+    title: ['Industrial Design, College of Design and Innovation, Tongji University', 'Candidate for Bachelor of Engineering | Cumulative GPA: 4.46'],
+    time: ['09.2015 - 06.2020']
+  }]
+}, {
+  title: 'DESIGN INTERNSHIP EXPERIENCE',
+  info: [{
+    title: ['Capgemini'],
+    subtitle: ['UX/UI Design Intern'],
+    desc: ['Co-created with clients to identify the opportunity and develop the service through design strategy', 'Collaborated with front-end engineers to improve user interface of digital products'],
+    time: ['11.2019 - Present']
+  }, {
+    title: ['Volvo Car Asia Pacific'],
+    subtitle: ['Design Color & Material Intern'],
+    desc: ['Completed benchmark and trend research on car exterior and interior color & material', 'Created pattern and color propasals for interior deco and leather monthly'],
+    time: ['02.2019 - 07.2019']
+  }, {
+    title: ['Neuni Group, Shanghai'],
+    subtitle: ['Creative and Visual Design Intern'],
+    desc: ['Proposed creative plans for products and installations adopting new materials', 'Led visual design for local exhibitions'],
+    time: ['05.2018 - 08.2018']
+  }]
+}, {
+  title: 'RESEARCH EXPERIENCE',
+  info: [{
+    title: ['“Next Mobility” Laboratory, College of Design and Innovation, Tongji University'],
+    subtitle: ['Researcher'],
+    desc: ["Composed paper on \u201CIndustrial Design Knowledge for Additive Manufacturing\u201D", "Composed paper on \u201CStudy on the Pedestrian Signage System in International Megacities\u201D"],
+    time: ['08.2018 - 12.2018']
+  }]
+}, {
+  title: 'COMPETITION EXPERIENCE',
+  info: [{
+    title: ['Closing the Loop on Food Packaging | ExxonMobil Challenge Reality Hackathon'],
+    subtitle: ['Gold Prize'],
+    time: ['2019']
+  }, {
+    title: ['EMO Robot Product Design | Shanghai Industry Design Competition'],
+    subtitle: ['First Prize'],
+    time: ['2018']
+  }, {
+    title: ['Bamboo Stool Design | The Second ‘Think Youth’ Shanghai Design Competition '],
+    subtitle: ['Second Prize'],
+    time: ['2017']
+  }, {
+    title: ['Unmanned Aerial Vehicle Design | Honeywell Internal Competition'],
+    subtitle: ['Third Prize'],
+    time: ['2017']
+  }, {
+    title: ['15% Academic Achievement Scholarship | College of Design and Innovation'],
+    time: ['2017']
+  }],
+  // 由于上下margin 重叠的规则，最后的Gap不会影响到存在大于 42 marginTop的下一个元素的margin展示，比如下一个是80，那么结果还是 42 & 80 = 80 而不会变成 122
+  bottomGap: '42rem'
+}, {
+  title: 'MANAGEMENT EXPERIENCE',
+  info: [{
+    title: ['The First China Industry Design Exhibition, Wuhan, China'],
+    subtitle: ['Exhibition Designer for Tongji University’s field'],
+    desc: ['Planned exhibition design, including showing forms and lighting design', 'Arranged exhibits’ collection and logistics from Shanghai to Wuhan'],
+    time: ['11.2017']
+  }, {
+    title: ['Tongji Design Week'],
+    subtitle: ['Best Performance Core Volunteer'],
+    desc: ['Communicated with 10 designers and artists to align exhibition plans', 'Coordinated with other volunteers to meet requests on space and activities '],
+    time: ['10.2016 & 2017']
+  }, {
+    title: ['Tongji University Student Union'],
+    subtitle: ['Deputy Minister of Human Resources Department'],
+    desc: ['Ran department to operate 12 events for more than 400 student union members', 'Met with administrators to address development of internal regulations weekly'],
+    time: ['09.2015 - 08.2016']
+  }]
+}];
+
+var VideoBlog = __jsx("div", {
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 150
+  },
+  __self: undefined
+}, __jsx("h1", {
+  style: {
+    fontFamily: 'Sofia '
+  },
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 151
+  },
+  __self: undefined
+}, "IXD VideoBlog + 12440483"), __jsx("video", {
+  src: '/static/videos/newnew.mp4',
+  autoPlay: false,
+  style: {
+    width: '1600rem',
+    marginTop: '20rem'
+  },
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 158
+  },
+  __self: undefined
+}));
+
+var Profile = function Profile() {
+  return __jsx("div", {
+    style: {
+      width: '1600rem'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 170
+    },
+    __self: this
+  }, VideoBlog, __jsx(_Banner__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 176
+    },
+    __self: this
+  }), __jsx(_Contact__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 177
+    },
+    __self: this
+  }), myInfo.map(function (info) {
+    return __jsx(_common_Pharse__WEBPACK_IMPORTED_MODULE_3__["default"], {
+      info: info.info,
+      title: info.title,
+      key: info.title,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 179
+      },
+      __self: this
+    });
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Profile);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.ba1b3b4a55bffd851e94.hot-update.js.map
